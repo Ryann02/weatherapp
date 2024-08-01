284da701cf665f1159fafeea49f96fb2f6b1b968
Ryann02 <138752405+Ryann02@users.noreply.github.com>
2024-08-01 19:17:37 +0700

    App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import Weather from "./Weather";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const API_KEY = "a08575a1805e3e3299c68b680e6a0160"; // Ganti dengan API key Anda
 
@@ -34,11 +35,23 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <input type="text" value={city} onChange={handleInputChange} placeholder="Enter city" />
-      <button onClick={handleSearch}>Search</button>
-      {weatherData && <Weather data={weatherData} />}
+    <div className="App d-flex justify-content-center align-items-center vh-100">
+      <div className="weather-container p-5 rounded shadow-lg  text-black">
+        <h1 className="text-center mb-4">Weather App</h1>
+        <div className="form-group">
+          <input
+            type="text"
+            value={city}
+            onChange={handleInputChange}
+            placeholder="Enter city"
+            className="form-control mb-3"
+          />
+          <button onClick={handleSearch} className="btn btn-dark w-100">
+            Search
+          </button>
+        </div>
+        {weatherData && <Weather data={weatherData} />}
+      </div>
     </div>
   );
 }
