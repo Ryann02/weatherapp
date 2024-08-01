b6709642f2d692fe5679b875fc8f05a69528d8e4
Ryann02 <138752405+Ryann02@users.noreply.github.com>
2024-08-01 19:18:20 +0700

    Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Weather({ data }) {
+  const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
   return (
-    <div className="Weather">
-      <h2>Weather in {data.name}</h2>
-      <p>Temperature: {data.main.temp} °C</p>
-      <p>Weather: {data.weather[0].description}</p>
+    <div className="Weather mt-4">
+      <h2 className="text-center">Weather in {data.name}</h2>
+      <div className="text-center">
+        <img src={iconUrl} alt={data.weather[0].description} />
+      </div>
+      <p className="text-center">Temperature: {data.main.temp} °C</p>
+      <p className="text-center">Weather: {data.weather[0].description}</p>
     </div>
   );
 }
